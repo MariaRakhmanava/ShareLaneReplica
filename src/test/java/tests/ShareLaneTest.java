b3c9897ff188ba85b659871cbbf1b76115d14091
MariaRakhmanava <mariah.rakhmanava@gmail.com>
2021-05-03 15:46:14 +0300

    Added chromedriver.exe, ShareLaneTest.java

---
+++ src/test/java/tests/ShareLaneTest.java
@@ -0,0 +1,47 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ShareLaneTest {
+    @Test
+    public void fillZipCodeFieldTest() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        driver.findElement(By.name("zip_code")).sendKeys("123456");
+        driver.findElement(By.xpath("//*[@value='Continue']")).click();
+        driver.quit();
+    }
+
+    @Test
+    public void registrationPositiveTest() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.sharelane.com/cgi-bin/register.py?page=1&zip_code=123456");
+        driver.findElement(By.name("first_name")).sendKeys("Jane");
+        driver.findElement(By.name("last_name")).sendKeys("Stone");
+        driver.findElement(By.name("email")).sendKeys("jane.stone@gmail.com");
+        driver.findElement(By.name("password1")).sendKeys("ijkl890%");
+        driver.findElement(By.name("password2")).sendKeys("ijkl890%");
+        driver.findElement(By.xpath("//*[@value='Register']")).click();
+        driver.quit();
+    }
+
+    @Test
+    public void registrationAllEmptyFieldsTest() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.sharelane.com/cgi-bin/register.py?page=1&zip_code=123456");
+        driver.findElement(By.name("first_name")).sendKeys("");
+        driver.findElement(By.name("last_name")).sendKeys("");
+        driver.findElement(By.name("email")).sendKeys("");
+        driver.findElement(By.name("password1")).sendKeys("");
+        driver.findElement(By.name("password2")).sendKeys("");
+        driver.findElement(By.xpath("//*[@value='Register']")).click();
+        driver.quit();
+    }
+
+}
